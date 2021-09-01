d5ec4df42efaee7937adb74a0c1c45e1c7576cdb
garvitkhamesra <garvitkhamesra@gmail.com>
2021-09-01 12:23:15 +0530

    OneAway, Bit vector palindromicpermutation, Stringompression

--- ctci/PalindromicPermutation.java
+++ ctci/PalindromicPermutation.java
@@ -25,8 +25,33 @@ public class PalindromicPermutation {
         return count <= 1;
     }
 
+    public static boolean isPermutationOfPalindromeBitVector(String str) {
+        str = str.toLowerCase();
+        int bitVector = 0;
+        for (char ch : str.toCharArray()) {
+            int val = getCharacterValueWithZeroIndex(ch);
+            int mask = (1 << val);
+            if (val >= 0) {
+                if ((bitVector & mask) == 0) bitVector |= mask;
+                else bitVector &= ~mask;
+            }
+        }
+        return bitVector == 0 | checkIfOnly1BitMasked(bitVector);
+    }
+
+    private static boolean checkIfOnly1BitMasked(int bitVector) {
+        return (bitVector & (bitVector - 1)) == 0;
+    }
+
+    private static int getCharacterValueWithZeroIndex(char ch) {
+        int val = ch - 'a';
+        if (val >= 0) return val;
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(PalindromicPermutation.isPermutationOfPalindrome("tacsdg sdcat"));
+        System.out.println(PalindromicPermutation.isPermutationOfPalindrome("tacsdgf sdcat"));
+        System.out.println(PalindromicPermutation.isPermutationOfPalindromeBitVector("tacsdgf sdcat"));
     }
 }
 
---
+++ ctci/StringCompression.java
@@ -0,0 +1,28 @@
+public class StringCompression {
+    /*
+        Question Explanation
+            The question is to return a compressed string. eg abcd -> abcd, aabbccddeeaaaa -> a2b2c2d2e2a4
+        Solution approaches
+            1. Use of stringbuilder is key to avoid new string creations
+                1.1 Time Complexity : O(N)
+                1.2 Space Complexity : O(N)
+    */
+
+    public static String getCompressedString(String str) {
+        StringBuilder compressedStr = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            count++;
+            if ((i+1) >= str.length() || str.charAt(i) != str.charAt(i+1)) {
+                compressedStr.append(str.charAt(i));
+                compressedStr.append(count);
+                count = 0;
+            }
+        }
+        return str.length() >= compressedStr.length() ? compressedStr.toString() : str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getCompressedString("abcd"));
+    }
+}
